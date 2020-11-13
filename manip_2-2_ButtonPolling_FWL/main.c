a3338909a1be4627b39ae2c813e0c2a067f869de
mohamedwaelbenismail <ahmedgrati1999@gmail.com>
2020-11-13 16:35:58 +0100

    manip2_2

--- manip_2-2_ButtonPolling_FWL/main.c
+++ manip_2-2_ButtonPolling_FWL/main.c
@@ -1,49 +1,47 @@
-
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
 
-/*----------------- Déclaration des structures relatives aux periphs utilisés ---------------*/
-GPIO_InitTypeDef  GPIO_InitStructure;
+GPIO_InitTypeDef GPIO_InitStructure;
 
+// variable d'appui
+uint16_t appui;
 /* ---------------Private functions ---------------*/
 void Delay(vu32 nCount);
-
-uint16_t appui; // variable appui pour lire l'etat du bouton
-
-int main(void)
-{ /*---------- Activer les horloges des ports utilsés (port A et port C)------------ */ 
- ...................... (........................................., .........);
-
-/*-------------- Configure the GPIO_LED pin -----------------*/
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9|GPIO_Pin_8;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz ;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP ;
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
-
-/*-------------- Configure the GPIO_ Pushbutton pin -----------------*/
-  GPIO_InitStructure.GPIO_Pin = ........;
-  GPIO_InitStructure.GPIO_Mode = ............... ; // IN Floating
-  GPIO_Init(......,...............);
-
-while (1)
- 	 {
-appui = ............................(GPIOA,..........); 
-
-if(appui)	         {
- GPIO_SetBits(GPIOC,GPIO_Pin_8|GPIO_Pin_9);
-  	          }
-else
-                      {
-  GPIO_ResetBits(GPIOC,GPIO_Pin_8|GPIO_Pin_9);
-                       }
-          }
-   }
-/*******************************************************************************
-* Function Name  : Delay
-* Description    : Inserts a delay time.
-* Input          : nCount: specifies the delay time length.
-*******************************************************************************/
-void Delay(vu32 nCount)
-{
-  for(; nCount != 0; nCount--);
+int main() {
+/*---------- Activer les horloges des ports utilsés (port A et port C)------------ */ 
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOA,ENABLE);
+	
+/*-------------- Configure the GPIO_LED (GPIOC) pin -----------------*/
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	
+/*-------------- Configure the GPIO_PushButton(GPIOA) pin -----------------*/
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	
+/*-------------- Initialization -----------------*/
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	while(1) {
+		// read the input in pin 0 of the GPIOA
+		appui = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0);
+		// Button is pushed ? LED ON : LED OFF
+		if(appui) {
+			// LED ON
+			GPIO_SetBits(GPIOC, GPIO_Pin_8 | GPIO_Pin_9); 
+		} else {
+			// LED OFF
+			GPIO_ResetBits(GPIOC, GPIO_Pin_8 | GPIO_Pin_9);
+		}
+	}
 }
+
+void Delay(vu32 nCount) {
+
+	int i;
+	for(i = nCount; i !=0 ; i-- );
+}
+
