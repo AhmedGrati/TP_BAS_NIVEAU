037008160315b68d5c494931eadf30e72e20ac04
mohamedwaelbenismail <ahmedgrati1999@gmail.com>
2020-11-13 14:29:17 +0100

    manip_2_1

--- manip_2-1_Led_FWL/ARM_MDK/Objects/main.d
+++ manip_2-1_Led_FWL/ARM_MDK/Objects/main.d
@@ -1,7 +1,7 @@
 .\objects\main.o: ..\main.c
 .\objects\main.o: ..\..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
 .\objects\main.o: ..\..\Libraries\CMSIS\CM3\CoreSupport\core_cm3.h
-.\objects\main.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\main.o: ..\..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\system_stm32f10x.h
 .\objects\main.o: ..\stm32f10x_conf.h
 .\objects\main.o: ..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_gpio.h
--- manip_2-1_Led_FWL/main.c
+++ manip_2-1_Led_FWL/main.c
@@ -1,38 +1,43 @@
-//GPIO_LEDS PROJECT
-
 #include "stm32f10x.h"
-
 /* declaration des structures relatives aux periphs utilisés -----------------------------------------------------------*/
-GPIO_InitTypeDef  GPIO_InitStructure;
-
+GPIO_InitTypeDef GPIO_InitStructure;
 
 /* Private functions ---------------------------------------------------------*/
 void Delay(vu32 nCount);
+main() {
+	
+	// activate gpioc rcc clock
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
+	
+	// we will work with pins 8 and 9
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
+	// set the mode out push pull	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	// set the speed to 2Mhz
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
 
+	// initialization of the gpioc 
+	GPIO_Init(GPIOC,&GPIO_InitStructure);
 
-int main(void)
-{  /* activer l'horloge du port c connecté aux leds */
-    RCC_APB2PeriphClockCmd (.................., ..........);
-
-    /* Configurer les pins PC8 et PC9 du port C */
-  GPIO_InitStructure.GPIO_Pin = ..................;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz ;
-  GPIO_InitStructure.GPIO_Mode = ............... ;// output Push Pull
-  GPIO_Init(........., .........);
-
-while (1)
-     {
-       //allumer des deux leds reliées aux bits PC8 et PC9
- GPIO_SetBits(.........., ..................);
- Delay (0xFFFFF);
-	 
-        // les éteindre
- ..............(........,.............);  
- Delay (0xFFFFF);
-     }
+	while(1) {
+		// set bit : pin 8 , 9 -> 1 : LED ON
+		GPIO_SetBits(GPIOC,GPIO_Pin_9 | GPIO_Pin_8);
+		
+		// delay
+		Delay(0xFFFFF);
+		
+		// reset bit : pin 8 , 9 -> 0 : LED OFF
+		GPIO_ResetBits(GPIOC,GPIO_Pin_9 | GPIO_Pin_8);
+		
+		
+		// delay
+		Delay(0xFFFFF);
+	}
 }
+
 /************** Function Name  : Delay  *****************/
 void Delay(vu32 nCount)
 {
-  for(; nCount != 0; nCount--);
+	int i;
+  for(i=nCount; i != 0; i--);
 }
